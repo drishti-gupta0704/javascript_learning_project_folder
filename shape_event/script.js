325db22c27e60bd76940a9ba468fb08cf45df32e
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-09-03 11:21:27 +0530

    adding behaviours of square 2

--- shape_event/script.js
+++ shape_event/script.js
@@ -1,3 +1,5 @@
+
+ //for square 1//
 let s1 = document.getElementById("sq1");
 s1.addEventListener("mouseenter",function(){
    let r = Math.floor(Math.random() * 100);
@@ -7,6 +9,28 @@ s1.addEventListener("mouseenter",function(){
 
 s1.addEventListener("mouseleave",function(){
     s1.innerHTML = `<h1>1</h1>`;
+})  
+
+//for square 2 //
+// on each hovering color changes ,total 3 color//
+let clr = "green"
+let s2 = document.getElementById("sq2");
+s1.addEventListener("mouseenter",function(){
+    if(clr =="green")   {
+        s2.style.backgroundColor = "green";
+        clr= "red" }
+    else if(clr=="red"){
+        s2.style.backgroundColor = "red" ;
+        clr= "blue";
+          }
+    else{
+        s2.style.backgroundColor = "blue";
+        clr = "green";
+    }
+
 })
 
+s2.addEventListener("mouseleave",function(){
+   s2.style.backgroundColor = "white";
+})  
 
