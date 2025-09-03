3f51add333238d364854017fd3f597f0fc3c7c0e
drishti-gupta0704 <drishti.gupta070424@gmail.com>
2025-09-03 11:40:08 +0530

    adding random color generation on hover for square 3 and fixed some bugs

--- shape_event/script.js
+++ shape_event/script.js
@@ -15,7 +15,7 @@ s1.addEventListener("mouseleave",function(){
 // on each hovering color changes ,total 3 color//
 let clr = "green"
 let s2 = document.getElementById("sq2");
-s1.addEventListener("mouseenter",function(){
+s2.addEventListener("mouseenter",function(){
     if(clr =="green")   {
         s2.style.backgroundColor = "green";
         clr= "red" }
@@ -34,3 +34,19 @@ s2.addEventListener("mouseleave",function(){
    s2.style.backgroundColor = "white";
 })  
 
+// square 3 generates  any random color on hovering //
+// rgb(a,b,c) where a,b,c lies between 0-255 //
+
+let s3 = document.getElementById("sq3");
+s3.addEventListener("mouseenter",function(){
+    let r1 = Math.floor(Math.random() * 256); // 256 is excluded
+    let r2 = Math.floor(Math.random() * 256);
+    let r3 = Math.floor(Math.random() * 256);
+    s3.style.backgroundColor= `rgb(${r1},${r2},${r3})`;
+
+
+})
+
+s3.addEventListener("mouseleave",function(){
+    s3.style.backgroundColor="white";
+})  
